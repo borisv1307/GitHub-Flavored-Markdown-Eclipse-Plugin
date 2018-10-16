bd0931ddffe67f3a31aeb5d0e901e6204c02e3f6
TianJiaFeng <940880136@qq.com>
2018-10-15 23:48:55 -0400

    use isEmpty() to replace StringUtils.isBlank()

--- src/main/java/onePipeLineFormat/StringFormatter.java
+++ src/main/java/onePipeLineFormat/StringFormatter.java
@@ -1,28 +1,26 @@
 package onePipeLineFormat;
 
-import org.junit.platform.commons.util.StringUtils;
-
 public class StringFormatter {
 
 	public String format(String string) {
-		if (StringUtils.isBlank(string)) { // input nothing
+		if (string.isEmpty()) { // input nothing
 			return "";
 		}
 		String original_line = string;
 		string = string.trim();
 		String formatted_line = "";
 		String[] temp = string.split("\\|", -1);
-		if (!StringUtils.isBlank(temp[0])) { // no beginning pipe
+		if (!temp[0].isEmpty()) { // no beginning pipe
 			return original_line;
 		}
 		for (int i = 1; i < temp.length - 1; i++) {
-			if (!StringUtils.isBlank(temp[i])) {
+			if (!temp[i].isEmpty()) {
 				temp[i] = " " + temp[i].trim() + " ";
 			} else {
 				temp[i] = " "; // |123||123| -> |123| | 123|
 			}
 		}
-		if (!StringUtils.isBlank(temp[temp.length - 1])) { // no closing pipe
+		if (!temp[temp.length - 1].isEmpty()) { // no closing pipe
 			temp[temp.length - 1] = " " + temp[temp.length - 1].trim();
 		}
 		formatted_line = String.join("|", temp);
