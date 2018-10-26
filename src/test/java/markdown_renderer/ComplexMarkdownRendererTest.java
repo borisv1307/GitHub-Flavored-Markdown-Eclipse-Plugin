a2b02d437b5bd8c59f042f59d18ac97c93f1e130
aw3227 <aw3227@drexel.edu>
2018-10-25 21:30:14 -0400

    changed naming conventions for all 4 complex test cases

--- src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
+++ src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
@@ -19,22 +19,22 @@ public class ComplexMarkdownRendererTest {
 	}
 
 	@Test
-	public void CombineExcessItalicsWithBold() {
+	public void combine_excess_italics_with_bold() {
 		readAndAssertFileContents("edgeCase_CombineExcessItalicsWithBold.html", "__***_**Test**_***__");
 	}
 
 	@Test
-	public void SevenPoundSymbols() {
+	public void seven_pound_symbols() {
 		readAndAssertFileContents("edgeCase_SevenPoundSymbols.html", "####### Test");
 	}
 
 	@Test
-	public void CombineItalicsWithBold() {
+	public void combine_italics_with_bold() {
 		readAndAssertFileContents("edgeCase_CombineItalicsWithBold.html", "_**Test**_");
 	}
 	
 	@Test
-	public void CombineUnorderedOrderedList() {
+	public void combine_unordered_ordered_list() {
 		readAndAssertFileContents("edgeCase_CombineUnorderedOrderedList.html", "1. * Test");
 	}
 
