d52a41a18bf267b825e140c37d4a14029b047eb6
aw3227 <aw3227@drexel.edu>
2018-10-27 17:49:27 -0400

    created test case for blockquote + unordered list + H3 and corresponding html file with GFM output Seems to have failed the test so I will not create pull request

--- src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
+++ src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
@@ -32,10 +32,15 @@ public class ComplexMarkdownRendererTest {
 	public void combine_italics_with_bold() {
 		readAndAssertFileContents("edgeCase_CombineItalicsWithBold.html", "_**Test**_");
 	}
-	
+
 	@Test
 	public void combine_unordered_ordered_list() {
 		readAndAssertFileContents("edgeCase_CombineUnorderedOrderedList.html", "1. * Test");
+	}
+
+	@Test
+	public void blockquote_unorderedlist_header_three() {
+		readAndAssertFileContents("edgeCase_BlockquoteUnorderedListH3.html", "> * ### Test");
 	}
 
 	private void readAndAssertFileContents(String filename, String contents) {
