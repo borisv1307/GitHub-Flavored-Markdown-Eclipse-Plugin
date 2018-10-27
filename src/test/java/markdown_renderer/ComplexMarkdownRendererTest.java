ff10a17311b7a41adfd56b26e68ae20df60d0ac3
aw3227 <aw3227@drexel.edu>
2018-10-27 17:15:56 -0400

    created test case for triple italics and corresponding .html file with GFM output

--- src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
+++ src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
@@ -32,10 +32,15 @@ public class ComplexMarkdownRendererTest {
 	public void combine_italics_with_bold() {
 		readAndAssertFileContents("edgeCase_CombineItalicsWithBold.html", "_**Test**_");
 	}
-	
+
 	@Test
 	public void combine_unordered_ordered_list() {
 		readAndAssertFileContents("edgeCase_CombineUnorderedOrderedList.html", "1. * Test");
+	}
+
+	@Test
+	public void triple_italics() {
+		readAndAssertFileContents("edgeCase_TripleItalics.html", "_*_Strike_*_");
 	}
 
 	private void readAndAssertFileContents(String filename, String contents) {
