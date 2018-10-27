343c475e075ead80f8e222dbd574e48e41452495
aw3227 <aw3227@drexel.edu>
2018-10-27 15:57:45 -0400

    includes test case for emphasis combined with inline code and .html file with GFM output

--- src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
+++ src/test/java/markdown_renderer/ComplexMarkdownRendererTest.java
@@ -32,10 +32,15 @@ public class ComplexMarkdownRendererTest {
 	public void combine_italics_with_bold() {
 		readAndAssertFileContents("edgeCase_CombineItalicsWithBold.html", "_**Test**_");
 	}
-	
+
 	@Test
 	public void combine_unordered_ordered_list() {
 		readAndAssertFileContents("edgeCase_CombineUnorderedOrderedList.html", "1. * Test");
+	}
+
+	@Test
+	public void emphasis_plus_inline_code() {
+		readAndAssertFileContents("edgeCase_EmphasisPlusInlineCode.html", "*`text`*");
 	}
 
 	private void readAndAssertFileContents(String filename, String contents) {
