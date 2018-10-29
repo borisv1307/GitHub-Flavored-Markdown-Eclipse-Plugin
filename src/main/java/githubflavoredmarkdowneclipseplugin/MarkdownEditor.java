3c92a858c4da4c7b738dc0a3025ed42cd9830bff
Kristófer Másson <kristoferma@me.com>
2018-10-29 16:11:36 -0400

    The editor now creates an html file and loads in the internal browser

--- src/main/java/githubflavoredmarkdowneclipseplugin/MarkdownEditor.java
+++ src/main/java/githubflavoredmarkdowneclipseplugin/MarkdownEditor.java
@@ -1,36 +1,88 @@
 package githubflavoredmarkdowneclipseplugin;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 
 import markdown_renderer.MarkdownRenderer;
 
 public class MarkdownEditor extends AbstractTextEditor {
 
-	public MarkdownEditor() {
+	private Activator activator;
+	private MarkdownRenderer markdownRenderer;
+
+	public MarkdownEditor() throws FileNotFoundException {
 
 		setSourceViewerConfiguration(new TextSourceViewerConfiguration());
 
 		setDocumentProvider(new TextFileDocumentProvider());
 
-		MarkdownRenderer markdownRenderer = new MarkdownRenderer();
+		// Activator manages connections to the Workbench
+		activator = Activator.getDefault();
+
+		markdownRenderer = new MarkdownRenderer();
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput editorInput) throws PartInitException {
+		super.init(site, editorInput);
+		IDocumentProvider documentProvider = this.getDocumentProvider();
+		IDocument document = documentProvider.getDocument(editorInput);
+
+		activator.log(markdownRenderer.render(document.get()));
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		IProject project = root.getProject("markdown");
+		IFolder folder = project.getFolder("html");
+		IFile file = folder.getFile("markdown.html");
+		try {
+			if (!project.exists())
+
+				project.create(null);
+
+			if (!project.isOpen())
+				project.open(null);
+			if (file.exists())
+				file.delete(true, null);
+			if (!file.exists()) {
+				byte[] bytes = document.get().getBytes();
+				InputStream source = new ByteArrayInputStream(bytes);
+				file.create(source, IResource.NONE, null);
+			}
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		IWebBrowser browser;
 		try {
-			Activator activator = Activator.getDefault();
 			browser = PlatformUI.getWorkbench().getBrowserSupport().createBrowser(activator.PLUGIN_ID);
 			URL url = FileLocator.find(activator.getBundle(), new Path("index.html"));
-			URL file = FileLocator.toFileURL(url);
-			browser.openURL(file);
+			URL htmlFile = FileLocator.toFileURL(file.getLocationURI().toURL());
+			browser.openURL(htmlFile);
 		} catch (PartInitException | IOException e) {
 			e.printStackTrace();
 		}
