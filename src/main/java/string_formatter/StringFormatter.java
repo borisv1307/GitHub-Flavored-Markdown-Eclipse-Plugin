0787c682958d53e491a1f2fbfa6c33e8d014c8fc
TianJiaFeng <940880136@qq.com>
2018-11-02 00:15:54 -0400

    new update

--- src/main/java/string_formatter/StringFormatter.java
+++ src/main/java/string_formatter/StringFormatter.java
@@ -32,7 +32,7 @@ public class StringFormatter {
 	// replace String.join()
 	private String join(String formattedLine, String[] temp, int length) {
 		for(int i = 0;i<length;i++) {
-			formattedLine += temp[i]+"|";
+			formattedLine = formattedLine + temp[i]+"|";
 		}
 		return formattedLine;
 	}
