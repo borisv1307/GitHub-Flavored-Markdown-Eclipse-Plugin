de3afb155aebe1396c8d32ea5119ed0d4b526649
IbukunF <irf23@drexel.edu>
2018-11-21 15:34:31 -0500

    fix formatting issues

--- src/main/java/preferences/PreferenceMonitor.java
+++ src/main/java/preferences/PreferenceMonitor.java
@@ -1,25 +1,23 @@
 package preferences;
 
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.IPreferenceStore;
 
 import githubflavoredmarkdowneclipseplugin.Activator;
 
-public class PreferenceMonitor implements IPreferenceChangeListener{
+public class PreferenceMonitor implements IPreferenceChangeListener {
 	private IPreferenceStore store;
-	
+
 	public PreferenceMonitor() {
 		store = Activator.getDefault().getPreferenceStore();
-    }
+	}
 
 	@Override
 	public void preferenceChange(PreferenceChangeEvent event) {
 		store = Activator.getDefault().getPreferenceStore();
 	}
-	
+
 	public boolean formatTable() {
 		return store.getBoolean("P_TABLE");
 	}
