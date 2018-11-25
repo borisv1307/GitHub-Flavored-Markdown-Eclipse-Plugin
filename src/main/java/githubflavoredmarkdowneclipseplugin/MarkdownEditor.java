8ea0385b1a457d44f3057c76575c90965329b8f1
IbukunF <irf23@drexel.edu>
2018-11-25 12:29:34 -0500

    create html file as temp file and set it to automatically delete when eclipse is closed

--- src/main/java/githubflavoredmarkdowneclipseplugin/MarkdownEditor.java
+++ src/main/java/githubflavoredmarkdowneclipseplugin/MarkdownEditor.java
@@ -1,29 +1,25 @@
 package githubflavoredmarkdowneclipseplugin;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IWorkbench;
@@ -49,7 +45,6 @@ public class MarkdownEditor extends AbstractTextEditor {
 	private StyledText styledText;
 	private Point point;
 	private IWebBrowser browser;
-	private IFolder folder;
 
 	public MarkdownEditor() throws FileNotFoundException {
 
@@ -90,44 +85,31 @@ public class MarkdownEditor extends AbstractTextEditor {
 		});
 	}
 
-	private IFile saveMarkdown(IEditorInput editorInput, IDocument document, IProgressMonitor progressMonitor) {
-		IProject project = getCurrentProject(editorInput);
-
+	private Path saveMarkdown(IEditorInput editorInput, IDocument document, IProgressMonitor progressMonitor) {
 		String mdFileName = editorInput.getName();
 		String fileName = mdFileName.substring(0, mdFileName.lastIndexOf('.'));
-		String htmlFileName = fileName + ".html";
-		folder = project.getFolder(".html");
-		IFile file = folder.getFile(htmlFileName);
+		Path file = null;
 
 		String markdownString = "<!DOCTYPE html>\n" + "<html>" + "<head>\n" + "<meta charset=\"utf-8\">\n" + "<title>"
-				+ htmlFileName + "</title>\n" + "</head>" + "<body>" + markdownRenderer.render(document.get())
+				+ fileName + ".html" + "</title>\n" + "</head>" + "<body>" + markdownRenderer.render(document.get())
 				+ "</body>\n" + "</html>";
 		try {
-			if (!project.isOpen())
-				project.open(progressMonitor);
-			if (!folder.exists()) {
-				folder.create(IResource.NONE, true, progressMonitor);
-			}
-			if (file.exists())
-				file.delete(true, progressMonitor);
-			if (!file.exists()) {
-				byte[] bytes = markdownString.getBytes();
-				InputStream source = new ByteArrayInputStream(bytes);
-				file.create(source, IResource.NONE, progressMonitor);
-			}
-		} catch (CoreException e) {
-			// TODO Auto-generated catch block
+			byte[] bytes = markdownString.getBytes();
+			file = Files.createTempFile(fileName, ".html");
+			Files.write(file, bytes);
+			file.toFile().deleteOnExit();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return file;
 	}
 
-	private void loadFileInBrowser(IFile file) {
+	private void loadFileInBrowser(Path file) {
 		IWorkbench workbench = PlatformUI.getWorkbench();
 		try {
 			if (browser == null)
 				browser = workbench.getBrowserSupport().createBrowser(Activator.PLUGIN_ID);
-			URL htmlFile = FileLocator.toFileURL(file.getLocationURI().toURL());
+			URL htmlFile = file.toUri().toURL();
 			browser.openURL(htmlFile);
 			IWorkbenchPartSite site = this.getSite();
 			IWorkbenchPart part = site.getPart();
@@ -149,7 +131,7 @@ public class MarkdownEditor extends AbstractTextEditor {
 		super.init(site, editorInput);
 		IDocumentProvider documentProvider = getDocumentProvider();
 		IDocument document = documentProvider.getDocument(editorInput);
-		IFile htmlFile = saveMarkdown(editorInput, document, null);
+		Path htmlFile = saveMarkdown(editorInput, document, null);
 		loadFileInBrowser(htmlFile);
 	}
 
@@ -197,7 +179,7 @@ public class MarkdownEditor extends AbstractTextEditor {
 
 			// Move the cursor
 			this.setHighlightRange(cursorLength, 0, true);
-			IFile htmlFile = saveMarkdown(editorInput, document, progressMonitor);
+			Path htmlFile = saveMarkdown(editorInput, document, progressMonitor);
 			loadFileInBrowser(htmlFile);
 			performSave(false, progressMonitor);
 		}
