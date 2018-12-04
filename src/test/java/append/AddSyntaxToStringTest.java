d95f08f6b2b3a894c58d0384447980ea565bf9af
borisv13 <borisv13@gmail.com>
2018-12-04 18:09:58 -0500

    Update AddSyntaxToStringTest.java

--- src/test/java/append/AddSyntaxToStringTest.java
+++ src/test/java/append/AddSyntaxToStringTest.java
@@ -126,14 +126,14 @@ public class AddSyntaxToStringTest {
 	}
 	
  	@Test
-	public void addUnorderedList() {
+	public void add_unordered_list() {
 		AddSyntaxToString addStuffToString = new AddSyntaxToString();
 		String actual = addStuffToString.addCharToEachLine("word\nword2\nword3", "*");
 		assertEquals("* word\n* word2\n* word3", actual);
 	}
  	
  	@Test
-	public void addCodeBlock() {
+	public void add_code_block() {
 		AddSyntaxToString addStuffToString = new AddSyntaxToString();
 		String actual = addStuffToString.codeBlock("word\nword2\nword3", "\t");
 		assertEquals("\n\tword\n\tword2\n\tword3\n", actual);
